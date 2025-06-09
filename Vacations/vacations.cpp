593ede76a12c8fa0b9876878becd6a026f6edc6f
anikatasnin <jarintasnin09@gmail.com>
2025-06-09 14:06:38 +0600

    added Vacations

--- Vacations/vacations.cpp
+++ Vacations/vacations.cpp
@@ -17,23 +17,21 @@ int main(){
         dp[i][2] = INT_MAX;
     }
 
-    dp[0][0] = 0;
-    dp[0][1] = 0;
-    dp[0][2] = 0;
+    dp[0][0] = dp[0][1] = dp[0][2] = 0;
 
     for(long long i = 1; i <= n; i++){
         dp[i][0] = 1 + min(dp[i-1][0], min(dp[i-1][1], dp[i-1][2]));
 
         if(a[i-1] == 1 || a[i-1] == 3){
-           dp[i][1] = min(dp[i-1][0], dp[i-1][2]);
+            dp[i][1] = min(dp[i-1][0], dp[i-1][2]);
         }
 
-        if(a[i-1] == 1 || a[i-1] == 3){
+        if(a[i-1] == 2 || a[i-1] == 3){
             dp[i][2] = min(dp[i-1][0], dp[i-1][1]);
         }
     }
 
-    cout << min(dp[n][0], min(dp[n][1], dp[n][2]));
+    cout << min({dp[n][0], dp[n][1], dp[n][2]});
 
     return 0;
 }
