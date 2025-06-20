19ee68fb4ee532b67360529683077b8d3b4280ad
anikatasnin <jarintasnin09@gmail.com>
2025-06-20 15:36:36 +0600

    added Exams

--- Exams/exams.cpp
+++ Exams/exams.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+
+    pair<int, int> a[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i].first >> a[i].second;
+
+    sort(a, a + n);
+
+    int d = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i].second >= d){
+            d = a[i].second;
+        }
+        else{
+            d = a[i].first;
+        }
+    }
+
+    cout << d << endl;
+
+    return 0;
+}
