881ae1a1dfc90f75eeeb7134d36f3b1ce0fcced4
anikatasnin <jarintasnin09@gmail.com>
2025-06-21 19:44:39 +0600

    added Pair of Topics

--- Pair of Topics/pairOfTopics.cpp
+++ Pair of Topics/pairOfTopics.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> a(n), b(n), c(n);
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    for(int i = 0; i < n; i++){
+        cin >> b[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        c[i] = a[i] - b[i];
+    }
+
+    sort(c.begin(), c.end());
+
+    long long ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (c[i] <= 0) continue;
+
+        int pos = lower_bound(c.begin(), c.end(), -c[i] + 1) - c.begin();
+
+        ans += (i - pos);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
 
