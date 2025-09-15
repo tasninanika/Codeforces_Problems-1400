512cc48f4798ab060ee06ebd730e16eba1c20220
anikatasnin <jarintasnin09@gmail.com>
2025-09-15 14:29:23 +0600

    added Amr and Pins

--- Amr and Pins/amrAndPins.cpp
+++ Amr and Pins/amrAndPins.cpp
@@ -1,1 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int r, x, y, x2, y2;
+    cin >> r >> x >> y >> x2 >> y2;
+
+    long long dx = x2 - x;
+    long long dy = y2 - y;
+    double dist = sqrt(dx * dx + dy * dy);
+
+    int steps = ceil(dist / (2.0 * r));
+
+    cout << steps << "\n";
+    return 0;
+}
 
