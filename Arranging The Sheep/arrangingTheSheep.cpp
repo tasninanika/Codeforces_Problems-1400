67dba07890933b236e7dd783a4203eea76d050ad
anikatasnin <jarintasnin09@gmail.com>
2025-09-16 18:37:11 +0600

    added Arranging The Sheep

--- Arranging The Sheep/arrangingTheSheep.cpp
+++ Arranging The Sheep/arrangingTheSheep.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    vector<int> sheeps;
+
+    for(int i = 0; i < n; i++) {
+        if(s[i] == '*') {
+            sheeps.push_back(i);
+        }
+    }
+
+    int index = (sheeps.size() - 1) / 2;
+
+    long long int ans = 0;
+
+    for(int i = 0; i < sheeps.size(); i++) {
+        ans += abs(sheeps[i] - (sheeps[index] - index + i));
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+}
 
