0569d55b3f05a434bfef3f362abad6b50edff896
anikatasnin <jarintasnin09@gmail.com>
2025-09-22 11:07:04 +0600

    added Orac and Models

--- Orac and Models/oracAndModels.cpp
+++ Orac and Models/oracAndModels.cpp
@@ -21,7 +21,9 @@ int main(){
 
         for(int i = 2; i <= n; i++){
             for(int j = 1; j * j <= i; j++){
+                if(i % j != 0){
 
+                }
             }
         }
     }
