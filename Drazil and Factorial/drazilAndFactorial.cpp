515d9e1152403d9a466dfbbcbf1726ce5bbb5e6a
anikatasnin <jarintasnin09@gmail.com>
2025-09-27 20:21:53 +0600

    added Drazil and Factorial

--- Drazil and Factorial/drazilAndFactorial.cpp
+++ Drazil and Factorial/drazilAndFactorial.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main(){
     map<char, string> mp;
 
+    mp['0'] = mp['1'] = ""; mp['2'] = "2"; mp['3'] = "3"; mp['4'] = "223"; mp['5'] = "5"; mp['6'] = "35"; mp['7'] = "7"; mp['8'] = "2227"; mp['9'] = "2337";
+
+    int n;
+
 
 
     return 0;
