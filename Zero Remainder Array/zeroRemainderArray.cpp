e5547a9eca920b7b54066f027479c7da61724813
anikatasnin <jarintasnin09@gmail.com>
2025-09-28 19:01:40 +0600

    added Zero Remainder Array

--- Zero Remainder Array/zeroRemainderArray.cpp
+++ Zero Remainder Array/zeroRemainderArray.cpp
@@ -1,43 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+void solve() {
+    int n,k;
+    cin >> n >> k;
+
+    map<int,int> fr;
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        int temp;
+        cin >> temp;
+
+        if(temp % k) {
+            fr[k - temp % k]++;
+            cnt++;
+        }
+    }
+
+    if(cnt == 0) {
+        cout << 0 << '\n';
+        return;
+    }
+
+    int ma = 0;
+    int rem;
+    for(auto [x,y] : fr) // O(n)
+    {
+        if(ma <= y) {
+            ma = y;
+            rem = x;
+        }
+    }
+
+    cout << 1LL * (ma - 1) * k + rem + 1 << '\n';
+}
+
+int main() {
     int t;
     cin >> t;
 
-    while(t--){
-        int n, k;
-        cin >> n >> k;
-
-        map<int,int> fr;
-        int cnt = 0;
-        for(int i = 0; i < n; i++){
-            int temp;
-            cin >> temp;
-
-            if(temp % k){
-                fr[k - temp % k]++;
-                cnt++;
-            }
-        }
-
-        if(cnt == 0){
-            cout << 0 << endl;
-            return;
-        }
-
-        int ma = 0;
-        int rem;
-
-        for(auto [x,y] : fr){
-            if(ma <= y){
-                ma = y;
-                rem = x;
-            }
-        }
-        cout << 1LL * (ma - 1) * k + rem + 1 << endl;
+    while(t--) {
+        solve();
     }
-
-
-    return 0;
 }
